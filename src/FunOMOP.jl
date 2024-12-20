ebf7e83098b6c56c536abfa1ac7550fb03aa11c3
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-19 20:10:52 -0500

    Include sample data as an artifact

--- src/FunOMOP.jl
+++ src/FunOMOP.jl
@@ -1,5 +1,13 @@
 module FunOMOP
 
-greet() = print("Hello World!")
+using CSV
+using DataFrames
+using DuckDB
+using HTTP
+
+function remote_test_csv(omop_table_name)
+	url = "https://raw.githubusercontent.com/AshlinHarris/OmopTestData/refs/heads/main/assets/omop-mimic-iv/1_omop_data_csv/$(omop_table_name).csv"
+	return HTTP.get(url).body |> CSV.File
+end
 
 end # module FunOMOP
