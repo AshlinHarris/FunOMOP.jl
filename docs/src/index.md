d0781d08fe3c22bb5a2feb141f0de55d57167122
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2024-12-23 09:56:01 -0500

    Add get_test_db_connection()

--- docs/src/index.md
+++ docs/src/index.md
@@ -2,10 +2,11 @@
 
 ```@meta
 DocTestSetup = quote
-	using DataFrames
-	using DuckDB
-	using FunOMOP
-	using FunSQL
+    using DataFrames
+    using DuckDB
+    using FunOMOP
+    using FunSQL
+    con = get_test_db_connection()
 end
 ```
 
@@ -15,7 +16,17 @@ Order   = [:function, :type]
 ```
 
 ```jldoctest
-julia> execute_query(@funsql person())
+julia> DBInterface.execute(con, "SELECT * FROM person LIMIT 1;") |> DataFrame
+1×18 DataFrame
+ Row │ person_id  gender_concept_id  year_of_birth  month_of_birth  day_of_bir ⋯
+     │ Int64      Int64              Int64          Int64           Int64      ⋯
+─────┼──────────────────────────────────────────────────────────────────────────
+   1 │         1               8507           1998               4             ⋯
+                                                              14 columns omitted
+```
+
+```jldoctest
+julia> DBInterface.execute(con, @funsql person()) |> DataFrame
 1×18 DataFrame
  Row │ person_id  gender_concept_id  year_of_birth  month_of_birth  day_of_bir ⋯
      │ Int64      Int64              Int64          Int64           Int64      ⋯
--- src/FunOMOP.jl
+++ src/FunOMOP.jl
@@ -1,6 +1,6 @@
 module FunOMOP
 
-export execute_query
+export get_test_db_connection
 export funsql_person
 export funsql_care_site
 export funsql_location
@@ -20,36 +20,34 @@ include("person.jl")
 include("provider.jl")
 
 """
-    execute_query(q)
-Run the FunSQL query against a sample data set
+    get_test_db_connection()
+Build a test database in memory and return the connection
 """
-function execute_query(q)
+function get_test_db_connection()
 
     con = DBInterface.connect(FunSQL.DB{DuckDB.DB}, ":memory:")
 
     #TODO: maybe name the schema?
-	# Parse the ddl into individual commands
-	pattern = r"CREATE TABLE @cdmDatabaseSchema.([\w_]*) \(([\s\S]*?)[\n\)];"
-	input_file_contents = read(joinpath(artifact"omop_ddl", "OMOPCDM_duckdb_5.4_ddl.sql"), String)
-	for match in eachmatch(pattern, input_file_contents)
+    # Parse the ddl into individual commands
+    pattern = r"CREATE TABLE @cdmDatabaseSchema.([\w_]*) \(([\s\S]*?)[\n\)];"
+    input_file_contents = read(joinpath(artifact"omop_ddl", "OMOPCDM_duckdb_5.4_ddl.sql"), String)
+    for match in eachmatch(pattern, input_file_contents)
 
-		# Create the table
-		command = replace(match.match,
-			"@cdmDatabaseSchema." => "",
-			"integer" => "BIGINT",
-		)
-		DBInterface.execute(con, command)
+        # Create the table
+        command = replace(match.match,
+            "@cdmDatabaseSchema." => "",
+            "integer" => "BIGINT",
+            )
+        DBInterface.execute(con, command)
 
-		# Write the data
-		table_name = match.captures |> first |> String |> uppercase
-		file_name = joinpath(artifact"synthea_omop_test", "Synthea27Nj_5.4", "$table_name.csv")
-		DBInterface.execute(con, "COPY $table_name FROM '$file_name';")
-	end
+        # Write the data
+        table_name = match.captures |> first |> String |> uppercase
+        file_name = joinpath(artifact"synthea_omop_test", "Synthea27Nj_5.4", "$table_name.csv")
+        DBInterface.execute(con, "COPY $table_name FROM '$file_name';")
+    end
 
-    #DBInterface.execute(con, "SHOW TABLES;") |> DataFrame |> x->x.name
-    #TODO: temporarily disabled
-    #DBInterface.execute(con, q) |> DataFrame
-    DBInterface.execute(con, "select * from person limit 1;") |> DataFrame
+    return con
+
 end
 
 end # module FunOMOP
