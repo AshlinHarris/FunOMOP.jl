5bb4d202ba345144d2f277fc48c24f5a955470b4
AshlinHarris <90787010+AshlinHarris@users.noreply.github.com>
2025-01-02 21:34:14 -0500

    Add fix.jl

---
+++ docs/fix.jl
@@ -0,0 +1,17 @@
+using Documenter
+using FunOMOP
+
+makedocs(
+    sitename = "FunOMOP",
+    format = Documenter.HTML(),
+    modules = [FunOMOP],
+    doctest = :fix,
+    #warnonly = true,
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/AshlinHarris/FunOMOP.jl.git",
+)
